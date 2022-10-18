7d5b950f556e47c7b37df816ba257d6eb1997204
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-10-19 00:53:25 +0530

    Update replacePi.cpp

--- Recursion/replacePi.cpp
+++ Recursion/replacePi.cpp
@@ -1,4 +1,4 @@
-Given a string, compute recursively a new string where all appearances of "pi" have been replaced by "3.14".
+/* Given a string, compute recursively a new string where all appearances of "pi" have been replaced by "3.14".
 Constraints :
 1 <= |S| <= 50
 where |S| represents the length of string S. 
@@ -6,7 +6,7 @@ Sample Input 1 :
 xpix
 Sample Output :
 x3.14x
-
+*/
 
 
 #include<cstring>
