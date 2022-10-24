990a3aecc8fc2925cca4d6d4f56a69068d1ecdef
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2022-10-24 13:37:31 +0530

    Update kth Largest element.cpp

--- C++ Standard Template Library I/kth Largest element.cpp
+++ C++ Standard Template Library I/kth Largest element.cpp
@@ -59,3 +59,27 @@ int main() {
 
     delete[] input;
 }
+/*
+#include<bits/stdc++.h>
+vector<int> kLargest(int arr[], int n, int k) {
+    vector<int>v;
+  /* sort(arr,arr+n,greater<int>());
+    for(int i=0;i<k;i++){
+        v.push_back(arr[i]);
+    }
+    return v;
+       
+    priority_queue<int>pq;
+    for(int i=0;i<n;i++){
+        pq.push(arr[i]);
+    }
+    for(int i=0;i<k;i++){
+        if(!pq.empty()){
+            v.push_back(pq.top());
+            pq.pop();
+            
+        }
+    }
+    return v;
+}
+*/
