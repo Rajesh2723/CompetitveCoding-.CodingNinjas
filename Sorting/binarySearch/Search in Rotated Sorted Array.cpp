ff4c4068eb940c86916fd87adfda280d3137b777
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2023-11-22 18:52:18 +0530

    Update Search in Rotated Sorted Array.cpp

--- Sorting/binarySearch/Search in Rotated Sorted Array.cpp
+++ Sorting/binarySearch/Search in Rotated Sorted Array.cpp
@@ -115,3 +115,29 @@ int main()
 		cout << search(arr, n, target) << "\n";
 	}
 } 
+/*
+int search(int* nums, int n, int target) {
+    
+   
+    int low = 0 , high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    
+}*/
