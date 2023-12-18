5944d88379c87cfaa0b56f8a1299e01b5d4392c4
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2023-12-18 15:50:50 +0530

    Create  Divisors of Factorial.cpp

---
+++ Number Theory II/Divisors of Factorial.cpp
@@ -0,0 +1,98 @@
+/*
+Given a number, find the total number of divisors of the factorial of the number.
+  Since the answer can be very large, print answer modulo 10^9+7.
+1 <= T <= 500
+0 <= N <= 50000
+input:3
+2
+3
+4
+  output:
+2
+4
+8
+  */
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mod = 1000000000 + 7;
+
+vector<int> primes;
+
+void preproccess(){
+    
+    int N = 50000;
+    int ar[N+1];
+    
+    for(int i=1;i<=N;i++) ar[i] = 1;
+    
+    ar[1] = 0;
+    
+    for(int i=2;i*i<=N; i++)
+        if(ar[i] == 1){
+            
+            for(int j=i*i;j<=N;j+=i)
+                ar[j] = 0;
+            
+        }
+    
+    for(int i=2;i<=N;i++)
+        if(ar[i] == 1)
+            primes.push_back(i);
+    
+}
+
+long long divisorCount(long long n){
+    
+    long long res = 1;
+    
+    for(int i=0;i<primes.size();i++)
+        if(primes[i]<=n){
+            
+            int p = primes[i];
+            int count = 0;
+            
+            while(p <= n) {
+                count = count + n / p;
+                p = p * primes[i];
+            }
+            
+            res = ((res % mod) * ((count + 1) % mod)) % mod;
+            
+        }
+    	else{
+            break;
+        }
+    
+    return res;
+    
+    
+}
+
+int main(){
+    
+    // write your code here
+    int t;
+    long long n;
+    
+    cin>>t;
+    
+    preproccess();
+    
+    while(t--){
+        cin>>n;
+        
+        cout<<divisorCount(n)<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
