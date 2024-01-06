82dae79355572e1f738c85bc28a9a304819710b7
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2024-01-06 12:14:36 +0530

    Update Sachin and Varun Problem.cpp

--- Number Theory II/Sachin and Varun Problem.cpp
+++ Number Theory II/Sachin and Varun Problem.cpp
@@ -1,19 +1,36 @@
 /*
+ Problem statement
 Varun and you are talking about a movie that you have recently watched while Sachin is busy teaching Number Theory. Sadly, Sachin caught Varun talking to you and asked him to answer his question in order to save himself from punishment. The question says:
+
 Given two weights of a and b units, in how many different ways you can achieve a weight of d units using only the given weights? Any of the given weights can be used any number of times (including 0 number of times).
+
+Since Varun is not able to answer the question, he asked you to help him otherwise he will get punished.
+
 Note: Two ways are considered different if either the number of times a is used or a number of times b is used is different in the two ways.
-input 1:
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 ≤ T ≤ 10 ^ 5
+
+1 ≤ a < b ≤ 10 ^ 9
+
+0 ≤ d ≤ 10 ^ 9
+Sample Input 1
 4
 2 3 7
 4 10 6
 6 14 0
 2 3 6
-
-  output:
+Sample Output 1
 1
 0
 1
-2*/
+2
+Explanation
+Test case 1: 7 can only be achieved by using 2 two times and 3 one time.
+
+Test case 2: 6 can't be achieved by using 4 and 10. So, 0 ways are there.
+  /
 #include<bits/stdc++.h>
 using namespace std;
 
