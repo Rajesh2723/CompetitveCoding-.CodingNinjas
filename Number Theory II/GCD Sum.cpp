40cf3e3c644dff9ba2572d6b9288f02ca2642b39
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2024-01-06 14:31:38 +0530

    Create GCD Sum.cpp

---
+++ Number Theory II/GCD Sum.cpp
@@ -0,0 +1,105 @@
+/*
+Problem statement
+Consider all numbers from 1 to ‘N’, your task is to find the sum of gcd of all pairs (i, j) such that 1 <= i < j <= N.
+
+For example for N = 3, all pairs are { (1, 2), (1, 3), (2, 3) }.
+
+Note :
+
+Gcd of two numbers (X, Y) is defined as the largest integer that divides both ‘X’ and ‘Y’. 
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+2
+3
+5
+Sample Output 1 :
+3
+11
+Explanation Of Sample Input 1 :
+Test case 1:
+gcd(1, 2) + gcd(1, 3) +  gcd(2, 3) = 1 + 1 + 1 = 3.
+
+Test case 2:
+gcd(1, 2) + gcd(1, 3) +  gcd(1, 4) + gcd(1, 5) +
+gcd(2, 3) + gcd(2, 4) + gcd(2, 5) +
+gcd(3, 4) + gcd(3, 5) +
+gcd(4, 5)  = 11.
+Sample Input 2 :
+1
+1
+Sample Output 2 :
+0
+Explanation Of Sample Input 2 :
+Test case 1:
+As there is no possible Paris, so the gcd sum is 0.
+  */
+/*
+    Time Complexity: O(N * log(N))
+    Space Complexity: O(N)
+
+    Where 'N' is the given number.
+*/
+
+// Function to calculate Euler's totient value for each 'i' from 1 to 'n'. 
+vector<int> eulerTotient(int n)
+{
+    vector<int> phi(n + 1);
+
+    for(int i= 1; i <= n; i++)
+    {
+        phi[i] = i;
+    }
+
+    for(int i = 2;i <= n; i++)
+    {
+        if(phi[i] == i)
+        {
+            for(int j = i; j <= n; j += i)
+            {
+                phi[j] /= i;
+                phi[j] *= (i - 1);
+            }
+        }
+    }
+
+    return phi;
+}
+
+// Function to calculate gcd sum of pairs (i, x) such that i <= x for all 'x'.
+vector<int> preCompute(int n, vector<int> &phi)
+{
+    // Declare an array to store 'f(x)' for all 'x' 1 to 'n'.
+    vector<int> pre(n + 1);
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = i; j <= n; j += i)
+        {
+            pre[j] += (i * phi[j / i]);
+        }
+    }
+
+    return pre;
+}
+
+long long gcdSum(int n)
+{
+    // Declare a variable to store gcd sum.
+    long long answer = 0;
+
+    // Declare an array 'phi' for Euler's totient fucntion values.
+    vector<int> phi = eulerTotient(n);
+
+    // Declare an array 'pre' to store 'f(x)' for each 'x' from 1 to 'n'.
+    vector<int> pre =  preCompute(n, phi);
+
+    // Find sum of each f(x) for all 'x' such that: 1 <= x <= n.
+    for (int i = 1; i <= n; i++)
+    {
+        answer += pre[i];
+        answer -= i;
+    }
+
+    // Return answer.
+    return answer;
+}
