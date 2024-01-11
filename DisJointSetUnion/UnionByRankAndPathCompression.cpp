3ffa8c0fe628b027cf8bd00505aaefe03482ae4e
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2024-01-11 12:21:34 +0530

    Create UnionByRankAndPathCompression.cpp

---
+++ DisJointSetUnion/UnionByRankAndPathCompression.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class DSU{
+	vector<int> parent;//parent node for each of them
+	vector<int> subtree_size;
+	int N;
+public:
+
+	DSU(int n){
+		N = n;
+		parent = vector<int>(n+1, 0);
+		subtree_size = vector<int>(n+1, 1);
+
+		for(int i=1; i<=n; i++) parent[i] = i;
+	}
+
+	int findRoot(int u){
+		while(u != parent[u]){
+			parent[u] = parent[parent[u]]; //Path compression
+			u = parent[u];
+		}
+
+		return u;
+	}
+
+
+	void combine(int u, int v){
+		int ru = findRoot(u);
+		int rv = findRoot(v);
+
+		if(ru == rv) return;
+
+		if(subtree_size[ru] > subtree_size[rv]){
+			parent[rv] = ru;
+			subtree_size[ru] += subtree_size[rv];
+		} else{
+			parent[ru] = rv;
+			subtree_size[rv] += subtree_size[ru];
+		}
+
+	}
+
+};
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+
+	int n;
+	cin>>n;
+
+	DSU dsu = DSU(n);
+
+	int q;
+	cin>>q;
+	while(q--){
+		int type, u, v;
+		cin>>type >> u >> v;
+
+		if(type == 1){
+			dsu.combine(u, v);
+		} else{
+			int ru = dsu.findRoot(u);
+			int rv = dsu.findRoot(v);
+			if(ru == rv){
+				cout<<"YES\n";
+			} else{
+				cout<<"NO\n";
+			}
+
+		}
+	}
+
+
+	return 0;
+}
