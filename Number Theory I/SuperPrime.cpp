dfa55d30c685ff477f1765632957e4ff817862cc
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2024-01-21 17:12:29 +0530

    Update SuperPrime.cpp

--- Number Theory I/SuperPrime.cpp
+++ Number Theory I/SuperPrime.cpp
@@ -1,7 +1,24 @@
 /*
+ Problem statement
 A number is called super-prime if it has exactly two distinct prime divisors
-input:10
-ouput:2
+
+Example 10 , 6
+
+You are supposed to find the count of super-prime numbers between 1 and N (inclusive).
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= N <= 10^6
+Sample Input 1:
+10
+Sample Output 1:
+2
+Sample Input 2:
+25
+Sample Output 2:
+10
+Explanation:
+The super-primes are: 6, 10, 12, 14, 15, 18, 20, 21, 22, 24
 */
  /*
   void* memset(void* ptr, int value, size_t num);
