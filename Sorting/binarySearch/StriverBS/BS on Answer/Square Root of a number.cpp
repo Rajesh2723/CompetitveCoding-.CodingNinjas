e57fd3b3921fa27af9995afeb2b674159d3e2103
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2024-03-05 19:01:56 +0530

    Create  Square Root of a number.cpp

---
+++ Sorting/binarySearch/StriverBS/BS on Answer/Square Root of a number.cpp
@@ -0,0 +1,54 @@
+/*
+Problem statement
+You are given a positive integer ‘n’.
+
+
+
+Your task is to find and return its square root. If ‘n’ is not a perfect square, then return the floor value of sqrt(n).
+
+
+
+Example:
+Input: ‘n’ = 7
+
+Output: 2
+
+Explanation:
+The square root of the number 7 lies between 2 and 3, so the floor value is 2.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+6
+Sample Output 1:
+2
+Explanation of Sample Input 1:
+The square root of the given number 6 lies between 2 and 3, so the floor value is 2.
+Sample Input 2:
+100
+Sample Output 2:
+10
+Explanation of Sample Output 2:
+The square root of the given number 100 is 10.
+Expected Time Complexity:
+Try solving this in O(log(n)).
+Constraints:
+0 <= n <= 10 ^ 9
+
+Time Limit: 1 sec.
+  */
+int floorSqrt(int n)
+{
+    int start=1;//range of values from 1 to n
+    int end=n;
+    while(start<=end){
+        long long   mid=start+(end-start)/2;
+        long long   ans=(mid*mid);
+        if(ans<=n){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+    return end;
+}
