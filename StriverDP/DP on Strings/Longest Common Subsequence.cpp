76ff6106922e7d359ffd23bc3461cc33fb25acfd
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2025-05-27 07:50:56 +0530

    Update Longest Common Subsequence.cpp

--- StriverDP/DP on Strings/Longest Common Subsequence.cpp
+++ StriverDP/DP on Strings/Longest Common Subsequence.cpp
@@ -36,3 +36,43 @@ int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
         return f(n-1,m-1,text1,text2,dp);
     }
 };
+//tabulation
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int f(string &s1,string &s2){
+    int n=s1.size();
+    int m=s2.size();
+    
+     vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+     
+     for(int i=0;i<=n;i++){
+         dp[i][0]=0;
+     }
+     for(int j=0;j<=m;j++){
+         dp[0][j]=0;
+     }
+     
+     for(int ind1=1;ind1<=n;ind1++){
+         for(int ind2=1;ind2<=m;ind2++){
+             if(s1[ind1-1]==s2[ind2-1]){
+                 dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+             }else{
+                 dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+             }
+         }
+     }
+     return dp[n][m];
+}
+
+int main()
+{
+   string s1,s2;
+   cin>>s1>>s2;
+   int n=s1.size();
+   int m=s2.size();
+   cout<<"String are coming here:"<<endl;
+   int count=f(s1,s2);
+   cout<<count<<endl;
+}
